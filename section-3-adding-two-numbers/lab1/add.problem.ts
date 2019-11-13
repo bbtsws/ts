3040117792432f5d142f1a69910cfcb2a1daf79b
TypeScript Workshop <56885773+bbtsws@users.noreply.github.com>
2019-11-13 10:22:09 +0000

    Update add.problem.ts

--- section-3-adding-two-numbers/lab1/add.problem.ts
+++ section-3-adding-two-numbers/lab1/add.problem.ts
@@ -4,6 +4,7 @@ type Zero = "zero";
 // @TODO: How can we define positive numbers?
 // Hint: look at the definition of "one" in line 14.
 type Pos = undefined;
+
 // @TODO: How can we define "numbers" as the union of Zero and all Positives?
 // Hint: how did we define Numbers in the previous lab?
 type Num = undefined;
@@ -11,14 +12,14 @@ type Num = undefined;
 const zero: undefined = "zero";
 
 // One is the number after zero
-const one: undefined = { prev: zero };
+const one: undefined = { "prev": zero };
 
 // @TODO: How do we define two?
 const two: undefined = undefined;
 
 // @TODO: How do we define the positive numbers?
 // Hint: zero us not a positive number
-const isPositive = (x: Num): boolean => undefined;
+const isZero = (x: Num): boolean => undefined;
 
 // @TODO: How do we define plus 1?
 // Hint: how did we define "one" based on "zero"?
@@ -32,17 +33,21 @@ const minus1 = (x: Pos): Num => undefined;
 // Hint: 
 // "a + b = (a + 1) + (b - 1)"
 // "c + 0 = c"
-const add = (x: undefined, y: undefined): undefined =>
+const add = (x: Num, y: Num): undefined =>
 	undefined;
 
 const eq = (x: Num, y: Num): boolean =>
-	x == zero && y == zero ? true :
-		x == zero || y == zero ? false :
+	isZero(x) && isZero(y) ? true :
+		isZero(x) || isZero(y) ? false :
 			eq(minus1(x), minus1(y));
 
-const isOneEqualsOnePlusZero = eq(one, add(one, zero));
-const isTwoEqualsOnePlusOne = eq(two, add(one, one));
 
-console.log("isOneEqualsOnePlusZero", isOneEqualsOnePlusZero); // do we get true?
-console.log("isTwoEqualsOnePlusOne", isTwoEqualsOnePlusOne); // do we get true?
+const isOneEqualsOnePlusZero = eq(one, add(one, zero));
+console.log("one = one + zero", isOneEqualsOnePlusZero); // true?
+
+const isTwoEqualsOnePlusOne = eq(two, add(one, one));
+console.log("two = one + one", isTwoEqualsOnePlusOne); // true?
+
+const isThreeEqualsOnePlusOne = eq(add(one, two), add(one, one));
+console.log("three = one + one", isThreeEqualsOnePlusOne); // false?
 
