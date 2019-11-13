f4bcf80dca4e6e98c921c77d8bb0e9564c1af4c2
TypeScript Workshop <56885773+bbtsws@users.noreply.github.com>
2019-11-13 11:49:53 +0000

    Update add.types.solution.ts

--- section-3-adding-two-numbers/lab2/add.types.solution.ts
+++ section-3-adding-two-numbers/lab2/add.types.solution.ts
@@ -27,3 +27,14 @@ type Res2 = Equals<Plus1<Two>, Add<One, Two>>; // Two++ = One + Two
 // False
 type Res3 = Equals<Plus1<Two>, Add<One, One>>; // Two++ = One + One
 
+// let two: Add<One, One> = { prev: { prev: "zero" } };
+// two = { prev: "zero" };
+
+// let three: Add<Two, One> = { prev: { prev: { prev: "zero" } } };
+// three = "zero";
+
+// const one = two.prev;
+
+// const four: Add<Two, Two> = { prev: three };
+
+// const five: Add<Two, Plus1<Two>> = { prev: four };
